4da0eedab4ef6cfc2b18dd188f35c18760030c26
vagner483 <vagnerreboucas@outlook.com>
2021-09-15 14:34:49 -0300

    Scriipt intstalação atualizada

--- R/Teste.R
+++ R/Teste.R
@@ -1,2 +1,10 @@
-Teste
-Bom dia
+# Sem vinheta 
+remotes :: install_github ( " Model-R / modleR " , build  =  TRUE )
+
+# Com vinheta 
+remotes :: install_github ( " Model-R / modleR " ,
+                            build  =  TRUE ,
+                            dependencies  =  TRUE ,
+                            build_opts  = c ( " - -no-resave-data " , " --no-manual " ),
+                            build_vignettes  =  TRUE )
+
